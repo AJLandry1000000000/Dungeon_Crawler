f5fb307c452fa5fa0b4095256a42b81850af84b0
Austin Landry <a.landry@student.unsw.edu.au>
2020-07-19 14:25:23 +1000

    Update Treasure.java

--- src/unsw/dungeon/entity/model/Treasure.java
+++ src/unsw/dungeon/entity/model/Treasure.java
@@ -4,7 +4,7 @@ import unsw.dungeon.entity.Direction;
 import unsw.dungeon.entity.Entity;
 import unsw.dungeon.entity.Interactable;
 
-public class Treasure extends Entity implements Interactable {
+public class Treasure extends Entity implements Interactable, Collectable {
 
     public Treasure(int x, int y) {
         super(x, y);
