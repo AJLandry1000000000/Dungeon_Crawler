68d8a9fb6a0a49bc247b90f021852fbaf309b7b0
Austin <A.landry@bigpond.com>
2020-08-03 01:30:47 +1000

    make level 4 & 5 goal stages

--- src/unsw/dungeon/MenuBarController.java
+++ src/unsw/dungeon/MenuBarController.java
@@ -77,14 +77,13 @@ public class MenuBarController {
         } else if (file.equals("level2.json")) {
             level2GoalScreen();
         } else if (file.equals("level3.json")) {
-            
-            
+            level3GoalScreen();
         } else if (file.equals("level4.json")) {
-
+            level4GoalScreen();
         } else if (file.equals("level5.json")) {
-
+            level5GoalScreen();
         } else if (file.equals("level6.json")) {
-
+            //level6GoalScreen();
         }
     }
 
@@ -152,6 +151,110 @@ public class MenuBarController {
         s.setScene(sc);
         s.show();
     }
+
+
+    public void level3GoalScreen() throws IOException {
+        // Create a new stage.
+        Stage s = new Stage();
+        s.setTitle("Goals");
+
+        // Load the goals for this level.
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("level3Goals.fxml"));
+        Parent root = loader.load();
+        Scene sc = new Scene(root);
+
+        ArrayList<Goal> level2Goals = this.dungeon.getGoalTypes();
+        for (Goal g : level2Goals) {
+            // Get each specific goal.
+            if (g.getClass().equals(GoalTreasure.class)) {
+                GoalTreasure gt = (GoalTreasure) g;
+                if (gt.isCompleted(this.dungeon)) {
+                    Image im = new Image((new File("images/success.png")).toURI().toString());
+                    ImageView i = (ImageView) sc.lookup("#treasureGoalMark");
+                    i.setImage(im);
+                }
+            } else if (g.getClass().equals(GoalEnemies.class)) {
+                GoalEnemies ge = (GoalEnemies) g;
+                if (ge.isCompleted(this.dungeon)) {
+                    Image im = new Image((new File("images/success.png")).toURI().toString());
+                    ImageView i = (ImageView) sc.lookup("#enemyGoalMark");
+                    i.setImage(im);
+                }
+            } else if (g.getClass().equals(GoalExit.class)) {
+                GoalExit gex = (GoalExit) g;
+                if (gex.isCompleted(this.dungeon)) {
+                    Image im = new Image((new File("images/success.png")).toURI().toString());
+                    ImageView i = (ImageView) sc.lookup("#exitGoalMark");
+                    i.setImage(im);
+                }
+            } 
+        }
+
+        // Show the stage.
+        root.requestFocus();
+        s.setScene(sc);
+        s.show();
+    }
+
+    public void level4GoalScreen() throws IOException {
+        // Create a new stage.
+        Stage s = new Stage();
+        s.setTitle("Goals");
+
+        // Load the goals for this level.
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("level4Goals.fxml"));
+        Parent root = loader.load();
+        Scene sc = new Scene(root);
+
+        ArrayList<Goal> level2Goals = this.dungeon.getGoalTypes();
+        for (Goal g : level2Goals) {
+            // Get each specific goal.
+            if (g.getClass().equals(GoalExit.class)) {
+                GoalExit gex = (GoalExit) g;
+                if (gex.isCompleted(this.dungeon)) {
+                    Image im = new Image((new File("images/success.png")).toURI().toString());
+                    ImageView i = (ImageView) sc.lookup("#exitGoalMark");
+                    i.setImage(im);
+                }
+            } 
+        }
+
+        // Show the stage.
+        root.requestFocus();
+        s.setScene(sc);
+        s.show();
+    }
+
+    public void level5GoalScreen() throws IOException {
+        // Create a new stage.
+        Stage s = new Stage();
+        s.setTitle("Goals");
+
+        // Load the goals for this level.
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("level3Goals.fxml"));
+        Parent root = loader.load();
+        Scene sc = new Scene(root);
+
+        ArrayList<Goal> level2Goals = this.dungeon.getGoalTypes();
+        for (Goal g : level2Goals) {
+            // Get each specific goal.
+            if (g.getClass().equals(GoalTreasure.class)) {
+                GoalTreasure gt = (GoalTreasure) g;
+                if (gt.isCompleted(this.dungeon)) {
+                    Image im = new Image((new File("images/success.png")).toURI().toString());
+                    ImageView i = (ImageView) sc.lookup("#treasureGoalMark");
+                    i.setImage(im);
+                }
+            } else if (g.getClass().equals(GoalBoulders.class)) {
+                GoalBoulders gb = (GoalBoulders) g;
+                if (gb.isCompleted(this.dungeon)) {
+                    Image im = new Image((new File("images/success.png")).toURI().toString());
+                    ImageView i = (ImageView) sc.lookup("#boulderGoalMark");
+                    i.setImage(im);
+                }
+            }
+        }
+    
     /*
     pass dungeon to menuBarController via DungeonController
     write a isGoalPassed(String) function
