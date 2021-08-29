065bd3cac8584e6c2307227abda4975702ea396c
Sean Smith <seaniksmith@gmail.com>
2021-08-30 01:35:33 +1000

    Fixed script

--- script.bash
+++ script.bash
@@ -1,3 +1,3 @@
 cd src/
 javac -cp "../lib/*" unsw/dungeon/*.java
-java --module-path ../lib --add-modules=javafx.controls unsw.dungeon.DungeonApplication
+java --module-path ../lib --add-modules=javafx.controls,javafx.fxml unsw.dungeon.DungeonApplication
